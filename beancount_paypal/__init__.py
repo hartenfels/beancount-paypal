234b9ae54196f61bcd7919cbfdd9c41b5c0c0c3c
Carsten Hartenfels <hartenfels@users.noreply.github.com>
2023-06-01 10:56:59 +0200

    WIP: rewrite

--- beancount_paypal/__init__.py
+++ beancount_paypal/__init__.py
@@ -1,23 +1,326 @@
-from beancount.core.number import D
+from beancount.core.number import D, round_to
 from beancount.ingest import importer
-from beancount.core import account
-from beancount.core import amount
-from beancount.core import flags
-from beancount.core import data
+from beancount.core.amount import Amount
+from beancount.core.flags import FLAG_OKAY
+from beancount.core.data import Balance, Posting, Transaction, new_metadata
 
 from dateutil.parser import parse
 from datetime import datetime, timedelta
 from contextlib import contextmanager
 
 import csv
+import re
 import os
 
 from . import lang
 
+
 @contextmanager
 def csv_open(filename):
-    with open(filename, newline='', encoding='utf-8-sig') as f:
+    with open(filename, newline="", encoding="utf-8-sig") as f:
         yield csv.DictReader(f, quotechar='"')
+
+
+def get_narration(row):
+    return row.get("subject") or row.get("note") or row.get("item_title") or None
+
+
+class EntryPosting:
+    def __init__(self, kind, account, amount, currency):
+        self.kind = kind
+        self.account = account
+        self.amount = amount
+        self.currency = currency
+
+
+class Entry:
+    def __init__(self, importer, txn_id, name, index, date, payee, narration, kind):
+        self.importer = importer
+        self.txn_id = txn_id
+        self.name = name
+        self.index = index
+        self.date = date
+        self.payee = payee
+        self.narration = narration
+        self.kind = kind
+        self.postings = []
+        self.conversions = []
+        self.gross = None
+        self.txn_ids = [txn_id]
+        self.parties = set()
+        self.invoice_numbers = set()
+
+    def add(self, kind, account, amount, currency):
+        self.postings.append(EntryPosting(kind, account, amount, currency))
+
+    def add_conversion(self, row):
+        self.conversions.append(row)
+
+    def add_metadata(self, row):
+        txn_id = row["txn_id"]
+        if not txn_id in self.txn_ids:
+            self.txn_ids.append(txn_id)
+
+        for party in [row["from"], row["to"]]:
+            if party and party not in self.importer.email_address:
+                self.parties.add(party)
+
+        invoice_number = row.get("invoice_number")
+        if invoice_number:
+            self.invoice_numbers.add(invoice_number)
+
+    def get_metadata(self):
+        metadata = {}
+        if self.kind:
+            metadata["type"] = self.kind
+        for i, txn_id in enumerate(self.txn_ids):
+            metadata["txnid{}".format("" if i == 0 else i + 1)] = txn_id
+        for i, party in enumerate(sorted(self.parties)):
+            metadata["party{}".format("" if i == 0 else i + 1)] = party
+        for i, invoice_number in enumerate(sorted(self.invoice_numbers)):
+            metadata["invoiceno{}".format("" if i == 0 else i + 1)] = invoice_number
+        return metadata
+
+    def _get_currency(self):
+        currencies = set(posting.currency for posting in self.postings)
+        if len(currencies) == 1:
+            return currencies.pop()
+        else:
+            raise ValueError(
+                "Transaction {} doesn't have a single currency: {}".format(
+                    self.txn_id, currencies
+                )
+            )
+
+    def _get_total(self, negative):
+        total = D(0)
+        for posting in self.postings:
+            if posting.kind != "fee":
+                total += abs(posting.amount)
+        return -total if negative else total
+
+    def _make_convert(self, c1, c2):
+        currency = self._get_currency()
+        if c1["currency"] == currency and c2["currency"] != currency:
+            foreign, own = c1, c2
+        elif c1["currency"] != currency and c2["currency"] == currency:
+            foreign, own = c2, c1
+        else:
+            raise ValueError(
+                "Transaction {} has invalid conversion currencies {}".format(
+                    self.txn_id, self.conversions
+                )
+            )
+
+        foreign_amount = D(foreign["gross"])
+        actual_amount = self._get_total(foreign_amount < D(0))
+        if foreign_amount != actual_amount:
+            raise ValueError(
+                "Transaction {} has invalid conversion amount {} != {}".format(
+                    self.txn_id, foreign_amount, actual_amount
+                )
+            )
+
+        own_amount = abs(D(own["gross"]))
+        own_currency = own["currency"]
+        rate = own_amount / abs(foreign_amount)
+
+        def convert(amount, _):
+            converted = amount * rate
+            if abs(abs(converted) - own_amount) <= 0.001:
+                amount = -own_amount if converted < D(0) else own_amount
+            else:
+                amount = int(converted * D(100) + D(0.5)) / D(100)
+            return Amount(amount, own_currency)
+
+        return convert
+
+        return lambda amount, _: Amount(amount * rate, own_currency)
+
+    def get_currency_conversion(self):
+        if len(self.conversions) == 0:
+            return lambda amount, currency: Amount(amount, currency)
+        elif len(self.conversions) == 2:
+            return self._make_convert(*self.conversions)
+        else:
+            raise ValueError(
+                "Transaction {} has {} conversion posting(s)".format(
+                    self.txn_id, len(self.conversions)
+                )
+            )
+
+
+class ExtractState:
+    def __init__(self, importer, filename):
+        self._importer = importer
+        self._name = filename.name
+        self._entries = []
+        self._entries_by_txn_id = {}
+        self._last_balance_row = None
+
+    def _handle_fee(self, row, entry):
+        fee = D(row["fee"])
+        if fee != 0:
+            entry.add(
+                "fee", self._importer.commission_account, abs(fee), row["currency"]
+            )
+
+    def _handle_from_checking(self, row, entry):
+        currency = row["currency"]
+        entry.add_metadata(row)
+        entry.add(
+            "from_checking",
+            self._importer.checking_account,
+            -1 * D(row["gross"]),
+            currency,
+        )
+        entry.add("to_paypal", self._importer.account, D(row["net"]), currency)
+        self._handle_fee(row, entry)
+
+    def _handle_to_checking(self, row, entry):
+        currency = row["currency"]
+        entry.add_metadata(row)
+        entry.add("from_paypal", self._importer.account, D(row["gross"]), currency)
+        entry.add(
+            "to_checking", self._importer.checking_account, -1 * D(row["net"]), currency
+        )
+        self._handle_fee(row, entry)
+
+    def _handle_receive(self, row, entry):
+        entry.narration = get_narration(row)
+        entry.add_metadata(row)
+        entry.add("receive", self._importer.account, D(row["net"]), row["currency"])
+        self._handle_fee(row, entry)
+
+    def _handle_send(self, row, entry):
+        entry.narration = get_narration(row)
+        entry.add_metadata(row)
+        entry.add("send", self._importer.account, D(row["net"]), row["currency"])
+        self._handle_fee(row, entry)
+
+    def _handle_currency_conversion(self, row, entry):
+        entry.add_conversion(row)
+
+    def _handle_row(self, index, row, entry):
+        language = self._importer.language
+        # Memos don't affect the balance.
+        if not language.txn_memo(row):
+            # Currency conversions must be buffered for later. Transactions that
+            # seem to have multiple postings in the CSV just get their first one
+            # handled, because the remaining ones are just internal movements.
+            if language.txn_currency_conversion(row):
+                self._handle_currency_conversion(row, entry)
+            elif not entry.postings:
+                if language.txn_from_checking(row):
+                    self._handle_from_checking(row, entry)
+                elif language.txn_to_checking(row):
+                    self._handle_to_checking(row, entry)
+                elif row["to"] in self._importer.email_address:
+                    self._handle_receive(row, entry)
+                else:
+                    self._handle_send(row, entry)
+
+    def _handle_new_row(self, index, row, kind):
+        entry = Entry(
+            importer=self._importer,
+            txn_id=row["txn_id"],
+            name=self._name,
+            index=index,
+            date=row["date"],
+            payee=row["name"],
+            narration=get_narration(row),
+            kind=kind,
+        )
+
+        self._handle_row(index, row, entry)
+        return entry
+
+    def _handle_reference_row(self, index, row, entry):
+        self._handle_row(index, row, entry)
+
+    def _extract_row(self, index, row):
+        txn_id = row["txn_id"]
+        ref_id = row["reference_txn_id"]
+        language = self._importer.language
+        entry = not language.txn_refund(row) and (
+            self._entries_by_txn_id.get(ref_id) or self._entries_by_txn_id.get(txn_id)
+        )
+        if entry:
+            self._handle_reference_row(index, row, entry)
+            self._entries_by_txn_id[txn_id] = entry
+        else:
+            entry = self._handle_new_row(index, row, language.txn_kind(row))
+            self._entries.append(entry)
+
+        self._entries_by_txn_id[txn_id] = entry
+        if ref_id:
+            self._entries_by_txn_id[ref_id] = entry
+
+    def extract(self, index, raw_row):
+        language = self._importer.language
+        row = language.normalize_keys(raw_row)
+        row["date"] = language.parse_date(row["date"]).date()
+        row["gross"] = language.decimal(row["gross"])
+        row["fee"] = language.decimal(row["fee"])
+        row["net"] = language.decimal(row["net"])
+
+        self._extract_row(index, row)
+
+        if "balance" in row:
+            self._last_balance_row = (index, row)
+
+    def _mangle_entry(self, entry):
+        txn = Transaction(
+            meta=new_metadata(entry.name, entry.index, entry.get_metadata()),
+            date=entry.date,
+            flag=FLAG_OKAY,
+            payee=entry.payee,
+            narration=entry.narration,
+            tags=set(),
+            links=set(),
+            postings=[],
+        )
+
+        convert = entry.get_currency_conversion()
+        for posting in entry.postings:
+            txn.postings.append(
+                Posting(
+                    account=posting.account,
+                    units=convert(posting.amount, posting.currency),
+                    cost=None,
+                    price=None,
+                    flag=None,
+                    meta=None,
+                )
+            )
+
+        return txn
+
+    def finish(self):
+        if self._importer.pre_process:
+            self._importer.pre_process(self._entries)
+
+        entries = [self._mangle_entry(entry) for entry in self._entries]
+
+        if self._last_balance_row:
+            language = self._importer.language
+            index, row = self._last_balance_row
+            balance = Balance(
+                new_metadata(self._name, index + 1),
+                row["date"] + timedelta(days=1),
+                self._importer.account,
+                Amount(D(language.decimal(row["balance"])), row["currency"]),
+                None,
+                None,
+            )
+            if self._importer.post_process:
+                self._importer.post_process(balance)
+            entries.append(balance)
+
+        if self._importer.post_process:
+            self._importer.post_process(entries)
+
+        return entries
 
 
 class PaypalImporter(importer.ImporterProtocol):
@@ -28,24 +331,19 @@ class PaypalImporter(importer.ImporterProtocol):
         checking_account,
         commission_account,
         language=None,
-        metadata_map=None,
-        keep_empty_metadata=True,
-        categorize=None
+        pre_process=None,
+        post_process=None,
     ):
         if language is None:
             language = lang.en()
-
-        if metadata_map is None:
-            metadata_map = language.metadata_map
 
         self.email_address = set(email_address)
         self.account = account
         self.checking_account = checking_account
         self.commission_account = commission_account
         self.language = language
-        self.metadata_map = metadata_map
-        self.keep_empty_metadata = keep_empty_metadata
-        self.categorize = categorize
+        self.pre_process = pre_process
+        self.post_process = post_process
 
     def file_account(self, _):
         return self.account
@@ -58,171 +356,18 @@ class PaypalImporter(importer.ImporterProtocol):
                     return False
 
                 row = self.language.normalize_keys(row)
-                if not (row['from'] in self.email_address or row['to'] in self.email_address):
+                if not (
+                    row["from"] in self.email_address or row["to"] in self.email_address
+                ):
                     return False
 
                 return True
             except (StopIteration, UnicodeDecodeError):
                 return False
 
-    def _categorize_payment(self, txn, raw_row, value, currency):
-        if self.categorize:
-            category = self.categorize(raw_row)
-            if category:
-                txn.postings.append(
-                    data.Posting(
-                        category,
-                        amount.Amount(-1*D(value), currency),
-                        None, None, None, None
-                    )
-                )
-
     def extract(self, filename):
-        entries = []
-        last_txn_id = None
-        last_net = None
-        last_currency = None
-        last_was_currency = False
-
+        state = ExtractState(self, filename)
         with csv_open(filename.name) as rows:
             for index, raw_row in enumerate(rows):
-                row = self.language.normalize_keys(raw_row)
-
-                # Disregard entries about invoices being sent. Merely sending
-                # an invoice doesn't affect balances, only their payment does,
-                # which will show up in a separate row.
-                if self.language.txn_invoice_sent(row):
-                    continue
-
-                metadata = { k: raw_row[v] for k, v in self.metadata_map.items()
-                        if self.keep_empty_metadata or raw_row.get(v) }
-
-                row['date'] = self.language.parse_date(row['date']).date()
-                row['gross'] = self.language.decimal(row['gross'])
-                row['fee'] = self.language.decimal(row['fee'])
-                row['net'] = self.language.decimal(row['net'])
-
-                if row['reference_txn_id'] != last_txn_id:
-                    meta = data.new_metadata(filename.name, index, metadata)
-
-                    txn = data.Transaction(
-                        meta=meta,
-                        date=row['date'],
-                        flag=flags.FLAG_OKAY,
-                        payee=row['name'],
-                        narration=row.get('item_title') or row.get('subject') or row.get('note'),
-                        tags=set(),
-                        links=set(),
-                        postings=[],
-                    )
-
-                if self.language.txn_from_checking(row):
-                    txn.postings.append(
-                        data.Posting(
-                            self.checking_account,
-                            amount.Amount(-1*D(row['gross']), row['currency']),
-                            None, None, None, None
-                        )
-                    )
-
-                    txn.postings.append(
-                        data.Posting(
-                            self.account,
-                            amount.Amount(D(row['net']), row['currency']),
-                            None, None, None, None
-                        )
-                    )
-
-                elif self.language.txn_to_checking(row):
-                    txn.postings.append(
-                        data.Posting(
-                            self.account,
-                            amount.Amount(D(row['gross']), row['currency']),
-                            None, None, None, None
-                        )
-                    )
-
-                    txn.postings.append(
-                        data.Posting(
-                            self.checking_account,
-                            amount.Amount(-1*D(row['net']), row['currency']),
-                            None, None, None, None
-                        )
-                    )
-
-                elif self.language.txn_currency_conversion(row):
-                    if last_was_currency:
-                        txn.postings.append(
-                            data.Posting(
-                                self.account,
-                                amount.Amount(D(last_net), last_currency),
-                                None, None, None, None
-                            )
-                        )
-                        txn.postings.append(
-                            data.Posting(
-                                self.account,
-                                amount.Amount(D(row['net']), row['currency']),
-                                None,
-                                amount.Amount(-1*(D(last_net) / D(row['net'])), last_currency),
-                                None, None
-                            )
-                        )
-                        last_net = None
-                        last_currency = None
-                        last_was_currency = False
-                    else:
-                        last_net = row['net']
-                        last_currency = row['currency']
-                        last_was_currency = True
-
-                elif row['to'] in self.email_address:
-                    txn.postings.append(
-                        data.Posting(
-                            self.account,
-                            amount.Amount(D(row['net']), row['currency']),
-                            None, None, None, None
-                        )
-                    )
-                    self._categorize_payment(txn, raw_row, row['gross'], row['currency'])
-
-                else:
-                    txn.postings.append(
-                        data.Posting(
-                            self.account,
-                            amount.Amount(D(row['gross']), row['currency']),
-                            None, None, None, None
-                        )
-                    )
-                    self._categorize_payment(txn, raw_row, row['net'], row['currency'])
-
-                if D(row['fee']) != 0:
-                    txn.postings.append(
-                        data.Posting(
-                            self.commission_account,
-                            amount.Amount(abs(D(row['fee'])), row['currency']),
-                            None, None, None, None
-                        )
-                    )
-
-                if row['reference_txn_id'] != last_txn_id:
-                    entries.append(txn)
-                    last_txn_id = row['txn_id']
-
-                last_currency = row['currency']
-                last_amount = amount
-
-        if 'balance' in row:
-            meta = data.new_metadata(filename.name, index + 1)
-            entries.append(
-                data.Balance(
-                    meta,
-                    row['date'] + timedelta(days=1),
-                    self.account,
-                    amount.Amount(D(self.language.decimal(row['balance'])), row['currency']),
-                    None,
-                    None,
-                )
-            )
-
-        return entries
+                state.extract(index, raw_row)
+        return state.finish()
--- beancount_paypal/lang.py
+++ beancount_paypal/lang.py
@@ -1,15 +1,18 @@
 from datetime import datetime
 
-class base():
+
+class base:
     # Paypal calls these "T-Codes", they identify a transaction type in a
     # language-agnostic way. See their developer documentation for a list.
     _txn_codes_from_checking = set(["T0300"])
     _txn_codes_to_checking = set(["T0400"])
     _txn_codes_currency_conversion = set(["T0200", "T0201", "T0202"])
-    _txn_codes_invoice_sent = set(["T7101"])
+    _txn_codes_refund = set(["T1107"])
 
     def identify(self, fields):
-        return all(elem in fields for elem in list(self.fields_map.keys())[:-5])  # last 5 keys are optional
+        return all(
+            elem in fields for elem in list(self.fields_map.keys())[:-6]
+        )  # last 6 keys are optional
 
     def _is_type(self, row, codes, title):
         return row.get("txn_code") in codes or row["txn_type"] == title
@@ -21,10 +24,21 @@ class base():
         return self._is_type(row, self._txn_codes_to_checking, self._to_checking)
 
     def txn_currency_conversion(self, row):
-        return self._is_type(row, self._txn_codes_currency_conversion, self._currency_conversion)
+        return self._is_type(
+            row, self._txn_codes_currency_conversion, self._currency_conversion
+        )
 
-    def txn_invoice_sent(self, row):
-        return self._is_type(row, self._txn_codes_invoice_sent, self._invoice_sent)
+    def txn_refund(self, row):
+        return self._is_type(row, self._txn_codes_refund, self._payment_refund)
+
+    def txn_memo(self, row):
+        return row["balance_impact"] == self._memo
+
+    def txn_kind(self, row):
+        if "txn_code" in row:
+            return "{} {}".format(row["txn_code"], row["txn_type"])
+        else:
+            return row["txn_type"]
 
     def decimal(self, data):
         return data
@@ -33,7 +47,7 @@ class base():
         return datetime.strptime(data, self._format)
 
     def normalize_keys(self, row):
-        return { self.fields_map.get(k, k):row[k] for k in row }
+        return {self.fields_map.get(k, k): row[k] for k in row}
 
 
 class en(base):
@@ -53,25 +67,23 @@ class en(base):
         "Transaction ID": "txn_id",
         "Reference Txn ID": "reference_txn_id",
         "Receipt ID": "receipt_id",
+        "Balance Impact": "balance_impact",
         # Optional keys:
         "Item Title": "item_title",
         "Subject": "subject",
         "Note": "note",
         "Balance": "balance",
         "Transaction Event Code": "txn_code",
-    }
-
-    metadata_map = {
-        "uuid": "Transaction ID",
-        "sender": "From Email Address",
-        "recipient": "To Email Address",
+        "Invoice Number": "invoice_number",
     }
 
     _format = "%d/%m/%Y"
     _from_checking = "Bank Deposit to PP Account "
     _to_checking = "General Withdrawal - Bank Transfer"
     _currency_conversion = "General Currency Conversion"
+    _payment_refund = "Payment Refund"
     _invoice_sent = "Invoice Sent"
+    _memo = "Memo"
 
     def decimal(self, data):
         return data.replace(".", "").replace(",", ".")
@@ -94,25 +106,23 @@ class de(base):
         "Transaktionscode": "txn_id",
         "Zugehöriger Transaktionscode": "reference_txn_id",
         "Empfangsnummer": "receipt_id",
+        "Auswirkung auf Guthaben": "balance_impact",
         # Optional keys:
         "Artikelbezeichnung": "item_title",
         "Betreff": "subject",
         "Hinweis": "note",
         "Guthaben": "balance",
         "Transaktionsereigniscode": "txn_code",
-    }
-
-    metadata_map = {
-        "uuid": "Transaktionscode",
-        "sender": "Absender E-Mail-Adresse",
-        "recipient": "Empfänger E-Mail-Adresse",
+        "Rechnungsnummer": "invoice_number",
     }
 
     _format = "%d.%m.%Y"
     _from_checking = "Bankgutschrift auf PayPal-Konto"
     _to_checking = "Allgemeine Abbuchung"
     _currency_conversion = "Allgemeine Währungsumrechnung"
+    _payment_refund = "Rückzahlung"
     _invoice_sent = "Rechnung gesendet"
+    _memo = "Memo"
 
     def decimal(self, data):
         return data.replace(".", "").replace(",", ".")
